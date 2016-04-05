15cc2937cded55186518ec37bcda5cdb3547ab62
dinever <dinever@live.cn>
2016-04-05 17:33:12 -0400

    Fixed database typo

--- db.py
+++ db.py
@@ -7,4 +7,4 @@ APP_DIR = os.path.dirname(__file__)
 
 urlparse.uses_netloc.append('postgres')
 url = urlparse.urlparse(os.environ['DATABASE_URL'])
-database = peewee.PostgresqlDatabase(name=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+database = peewee.PostgresqlDatabase(url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
