cf06fbaea76088e66d6fa7f570485749675766c3
dinever <dinever@live.cn>
2016-04-06 10:54:17 -0400

    Adding server.py

---
+++ server.py
@@ -0,0 +1,15 @@
+import os
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+if __name__ == '__main__':
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
+
+    from analyticpi import database, app, migration
+
+    migration.initialize()
+    app.debug = bool(os.environ.get('DEBUG', True))
+    app.run(port=int(os.environ['PORT']))
+    database.close()
