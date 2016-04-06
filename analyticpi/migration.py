9705b66859b7e2dce230b08813beb03a7421d6f5
dinever <dinever@live.cn>
2016-04-06 10:54:51 -0400

    Added migration.py

--- analyticpi/migration.py
+++ analyticpi/migration.py
@@ -1,6 +1,15 @@
 from db import database
 from page_view import PageView
 
-if __name__ == '__main__':
+
+def initialize():
+    database.create_tables([PageView], safe=True)
+
+
+def migrate():
     database.create_tables([PageView], safe=True)
     database.close()
+
+
+if __name__ == '__main__':
+    migrate()
