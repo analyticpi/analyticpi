2ef257f48a530d9f685e6dd69a00a4b79a3ac836
dinever <dinever@live.cn>
2016-04-06 12:49:49 -0400

    Fixed migration.

--- analyticpi/migration.py
+++ analyticpi/migration.py
@@ -6,10 +6,5 @@ def initialize():
     database.create_tables([PageView], safe=True)
 
 
-def migrate():
-    database.create_tables([PageView], safe=True)
-    database.close()
-
-
 if __name__ == '__main__':
-    migrate()
+    initialize()
