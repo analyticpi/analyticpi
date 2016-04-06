76c22b9c13501a7f0f994c9cb408e99b31034cef
dinever <dinever@live.cn>
2016-04-06 09:55:18 -0400

    Using MySQL for local and Postgres for Heroku

--- db.py
+++ db.py
@@ -1,10 +1,21 @@
 import os
-import urlparse
 
 import peewee
 
 APP_DIR = os.path.dirname(__file__)
 
-urlparse.uses_netloc.append('postgres')
-url = urlparse.urlparse(os.environ['DATABASE_URL'])
-database = peewee.PostgresqlDatabase(url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+try:
+    import urlparse
+    import psycopg2
+    urlparse.uses_netloc.append('postgres')
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
+    database = peewee.PostgresqlDatabase(database=url.path[1:],
+                                         user=url.username,
+                                         password=url.password,
+                                         host=url.hostname,
+                                         port=url.port)
+except KeyError:
+    database = peewee.MySQLDatabase(os.environ["MYSQL_DATABASE"],
+                                    os.environ["MYSQL_HOST"],
+                                    user=os.environ["MYSQL_USER"],
+                                    passwd=os.environ["MYSQL_PASSWD"])
