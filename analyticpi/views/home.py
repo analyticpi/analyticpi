4e1225844d98b62c57edaa8bc51d06ceeb5d5745
dinever <dingpeixuan911@gmail.com>
2016-05-02 15:15:18 -0400

    Add landing page, formalize front-end development.

--- analyticpi/views/home.py
+++ analyticpi/views/home.py
@@ -1,8 +1,7 @@
-from flask import render_template
-from flask import Response
+from flask import render_template, url_for
+from flask import Response, redirect
 
 from analyticpi.models.page_view import PageView
-from analyticpi.reports import page_views, unique_ips, top_pages
 from analyticpi.views import home_view
 
 from flask.ext.login import current_user
@@ -21,12 +20,10 @@ def get_query(start, end):
 
 @home_view.route('/')
 def home():
-    query = get_query(None, None)
-    return render_template('index.html',
-                           page_views=page_views(query),
-                           unique_ips=unique_ips(query),
-                           top_pages=top_pages(query, 20))
-
+    if current_user.is_anonymous:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('site.show'))
 
 @home_view.errorhandler(404)
 def not_found(e):
---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  jshint     = require('gulp-jshint'),
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  sourcemaps = require('gulp-sourcemaps'),
+  livereload = require('gulp-livereload');
+
+gulp.task('html', function() {
+  gulp.src('../*.html')
+  .pipe(livereload());
+});
+
+gulp.task('css', function() {
+  gulp.src('src/scss/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('analyticpi/static/css/'))
+  .pipe(livereload());
+});
+
+gulp.task('js', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('app.js'))
+      //only uglify if gulp is ran with '--type production'
+      .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('analyticpi/static/js'))
+    .pipe(livereload());
+});
+
+gulp.task('vendor-js', function() {
+  return gulp.src([
+    'src/vendor/js/codemirror.js',
+    'src/vendor/js/*.js'
+  ])
+  .pipe(concat('vendor.js'))
+  .pipe(gulp.dest('analyticpi/static/js/'))
+});
+
+gulp.task('vendor-css', function() {
+  return gulp.src('src/vendor/css/*.css')
+  .pipe(concat('vendor.css'))
+  .pipe(gulp.dest('analyticpi/static/css/'))
+});
+
+gulp.task('copy-fonts', function() {
+  return gulp.src('src/fonts/**/*').pipe(gulp.dest('analyticpi/static/fonts/'));
+});
+
+gulp.task('copy-img', function() {
+  return gulp.src('src/img/**/*').pipe(gulp.dest('analyticpi/static/img/'));
+});
+
+//Watch task
+gulp.task('default',function() {
+  livereload.listen();
+  gulp.watch('src/scss/**/*.scss',['css']);
+  gulp.watch('src/js/**/*.js',['js']);
+  gulp.watch('src/vendor/css/*.css',['vendor-css']);
+  gulp.watch('src/vendor/js/*.js',['vendor-js']);
+  gulp.watch('src/fonts/**/*',['copy-fonts']);
+  gulp.watch('src/img/**/*.png',['copy-img']);
+  gulp.watch('../*.html',['html']);
+});
